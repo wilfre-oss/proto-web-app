432a4f57c1180f2e2ee98380fa63c72a4d1cd1d0
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2024-06-19 18:09:12 +0200

    spinners

---
+++ src/components/ui/Spinners.jsx
@@ -0,0 +1,4 @@
+
+export const Spinner = () => <div className="spinner"/>
+export const SmallSpinner = () => <div className="small-spinner"/>
+export const BigSpinner = () => <div className="big-spinner"/>
--- src/presenters/Category.jsx
+++ src/presenters/Category.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import { BigSpinner, SmallSpinner, Spinner } from "../components/ui/Spinners"
 
 const Category = () => {
   let { category } = useParams()
@@ -6,6 +7,9 @@ const Category = () => {
   return (
     <h1>
       {category}
+      <SmallSpinner />
+      <Spinner />
+      <BigSpinner />
     </h1>
   )
 }
